04c669435234faf0a4d6faa2b289f1308b072abf
Kyra van Giessel <110289@st-maartenscollege.nl>
2020-09-07 15:13:34 +0200

    hij doet het weer op magische wijze!! yahy

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,22 +8,22 @@ using SchoolTemplate.Models;
 
 namespace SchoolTemplate.Controllers
 {
-  public class HomeController : Controller
-  {
-    // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
-    string connectionString = "Server=172.16.160.21;Port=3306;Database=110289;Uid=110289;Pwd=orUceAKi;";
-
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-      List<Festival> Festival = new List<Festival>();
-      // uncomment deze regel om producten uit je database toe te voegen
-        Festival = GetFestivals();
+        // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
+        string connectionString = "Server=172.16.160.21;Port=3306;Database=110289;Uid=110289;Pwd=orUceAKi;";
 
-      return View(Festival);
-    }
+        public IActionResult Index()
+        {
+            List<Festival> Festival = new List<Festival>();
+            // uncomment deze regel om producten uit je database toe te voegen
+            Festival = GetFestivals();
+
+            return View(Festival);
+        }
 
 
-    private List<Festival> GetFestivals()
+        private List<Festival> GetFestivals()
         {
             List<Festival> festivals = new List<Festival>();
 
@@ -53,58 +53,17 @@ namespace SchoolTemplate.Controllers
 
         }
 
-
-        public IActionResult Index()
-        {
-            List<Festival_dag> Festivaldag = new List<Festival_dag>();
-            // uncomment deze regel om producten uit je database toe te voegen
-            Festival_dag = GetFestivaldagen();
-
-            return View(Festival_dag);
-        }
-
-
-        private List<Festival_dag> GetFestivaldagen()
-        {
-            List<Festival_dag> festivaldagen = new List<Festival_dag>();
-
-            using (MySqlConnection conn = new MySqlConnection(connectionString))
-            {
-                conn.Open();
-                MySqlCommand cmd = new MySqlCommand("select * from Festivaldagen", conn);
-
-                using (var reader = cmd.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        Festivaldag F = new Festivaldag
-                        {
-                            Id = Convert.ToInt32(reader["Id"]),
-                            Naam = reader["Naam"].ToString(),
-                            Beschrijving = reader["Beschrijving"].ToString(),
-                            Datum = DateTime.Parse(reader["Datum"].ToString()),
-                            Prijs = Decimal.Parse(reader["Prijs"].ToString())
-                        };
-                        festivaldagen.Add(FD);
-                    }
-                }
-            }
-
-            return festivaldagen;
-
-        }
-
         [Route("festival/{id}")]
-        public IActionResult Festival (string id)
+        public IActionResult Festival(string id)
         {
             var model = GetFestival(id);
             return View(model);
         }
 
-        private Festival GetFestival (string id)
-        { 
-            
-            List<Festival> festivals= new List<Festival>();
+        private Festival GetFestival(string id)
+        {
+
+            List<Festival> festivals = new List<Festival>();
 
             using (MySqlConnection conn = new MySqlConnection(connectionString))
             {
@@ -128,7 +87,7 @@ namespace SchoolTemplate.Controllers
                 }
             }
 
-            return festivals[0]; 
+            return festivals[0];
         }
 
         public IActionResult Privacy()
@@ -153,9 +112,9 @@ namespace SchoolTemplate.Controllers
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
-  }
 }
